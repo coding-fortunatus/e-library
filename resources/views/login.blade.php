4e82997a0a08de2a14e0d30b27088534cc6a640f
Fortunatus Adegoke <fortunatusoluwadara@gmail.com>
2024-10-28 11:27:04 +0100

    New commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,22 +2,28 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 		<head>
-				<meta charset="UTF-8">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<meta http-equiv="X-UA-Compatible" content="ie=edge">
-				<title>Login Page - E-library NBU</title>
+				<meta charset="utf-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+				<meta name="description" content="">
+				<meta name="author" content="">
 				<link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
+				<title>Login Page - E-library NBU</title>
 				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 				<link rel="stylesheet" href="{{ asset('/css/login.css') }}">
+				<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+				@livewireStyles
 		</head>
+
+		@livewireScripts
 
 		<body>
 				<div class="container-fluid">
 						<div class="row">
 								<div class="col-lg-6 login-section-wrapper">
 										{{-- <div class="brand-wrapper">
-														<img src="{{ asset('img/NBU_Official-Logo.png') }}" class="img-fluid img-thumbnail" alt="NBU Logo">
-												</div> --}}
+												<img src="{{ asset('img/NBU_Official-Logo.png') }}" class="img-fluid img-thumbnail" alt="NBU Logo">
+										</div> --}}
 										<div class="login-wrapper my-auto card shadow-sm mx-auto p-4 border-0">
 												<h1 class="login-title">Log in</h1>
 												<form action="">
@@ -33,7 +39,7 @@
 												</form>
 												{{-- <a wire:navigate href="" class="forgot-password-link mb-3">Forgot password?</a> --}}
 												<p class="login-wrapper-footer-text">Don't have an account?
-														<a wire:navigate href="{{ route('welcome') }}" class="text-reset">Register here</a>
+														<a href="{{ route('welcome') }}" class="text-reset">Register here</a>
 												</p>
 										</div>
 								</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,22 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 		<head>
-				<meta charset="utf-8" />
-				<meta content="width=device-width, initial-scale=1.0" name="viewport" />
-				<title>Welcome - Nigerian British University Elibrary</title>
-				<meta name="description" content="" />
-				<meta name="keywords" content="" />
-
-				<!-- Fonts -->
+				<meta charset="utf-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+				<meta name="description" content="">
+				<meta name="author" content="">
 				<link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
-				<!-- Vendor CSS Files -->
+				<title>Welcome - Nigerian British University Elibrary</title>
 				<link href="{{ asset('/vendor/bootstrapes/css/bootstrap.min.css') }}" rel="stylesheet" />
 				<link href="{{ asset('/vendor/aoses/aos.css') }}" rel="stylesheet" />
 				<link href="{{ asset('/css/landing.css') }}" rel="stylesheet" />
+				<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+				@livewireStyles
 		</head>
 
 		<body class="index-page">
+				@livewireScripts
 				<main class="main">
 						<!-- Hero Section -->
 						<section id="hero" class="hero section dark-background">
@@ -31,15 +32,14 @@
 														</h2>
 														<p data-aos="fade-up" data-aos-delay="200">
 																We offer a wide range of academic resources, ensuring
-																easy access to textbooks, research papers, and digital
-																content through a user-friendly interface.
+																easy access to textbooks, research papers, and other digital
+																materials through a user-friendly interface.
 														</p>
 												</div>
 												<div class="col-lg-5 mt-3 mx-auto" data-aos="fade-up" data-aos-delay="300">
 														<form class="row row-cols-lg-auto g-3 align-items-center">
 																<div class="input-group">
-																		<input autofocus type="text" title="User ID" class="form-control"
-																				placeholder="Enter your user ID" />
+																		<input type="text" title="User ID" class="form-control" placeholder="Enter your user ID" />
 																</div>
 																<div class="input-group">
 																		<select class="form-select" name="role" aria-label="Account user type">
@@ -73,6 +73,7 @@
 				<script src="{{ asset('/vendor/bootstrapes/js/bootstrap.bundle.min.js') }}"></script>
 				<script src="{{ asset('/vendor/aoses/aos.js') }}"></script>
 				<script src="{{ asset('/js/landing.js') }}"></script>
+
 		</body>
 
 </html>
